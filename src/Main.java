664d595bcd7206fcac7c5365ec7b867c3eb70d60
baxynl <abdelghanielaoud@gmail.com>
2024-01-03 18:48:48 +0100

    Built Calculator Swing application with Nimbus look and feel. Adjusted UI layout for improved aesthetics and usability, including refining the positions and appearance of buttons. Committing changes for enhanced user experience.

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+            if ("Nimbus".equals(info.getName())) {
+                try {
+                    UIManager.setLookAndFeel(info.getClassName());
+                } catch (ClassNotFoundException e) {
+                    throw new RuntimeException(e);
+                } catch (InstantiationException e) {
+                    throw new RuntimeException(e);
+                } catch (IllegalAccessException e) {
+                    throw new RuntimeException(e);
+                } catch (UnsupportedLookAndFeelException e) {
+                    throw new RuntimeException(e);
+                }
+                break;
+            }
+        }
+        Calculator calculator = new Calculator();
+    }
+}
